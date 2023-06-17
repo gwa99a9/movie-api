a9fe3e581ffc1e6c977f0d9f3e59e18f17f59bc7
Jeewaka Manohara <jamesheller.jeewake@gmail.com>
2023-06-17 20:19:29 +0530

    refactor

--- src/main/java/com/gsd/movieapi/MovieApiApplication.java
+++ src/main/java/com/gsd/movieapi/MovieApiApplication.java
@@ -2,25 +2,11 @@ package com.gsd.movieapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class MovieApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MovieApiApplication.class, args);
     }
-
-    @GetMapping("/")
-    public String apiHome() {
-        return "Hello Word";
-    }
-
-    @GetMapping("/root")
-    public String apiRoot() {
-        return "endpoint /root";
-    }
-
 }
